58ae6c9754dbf189647855655719dd185a9ec04e
Tomi Chen <tctree333@users.noreply.github.com>
2021-05-29 20:19:03 -0700

    add vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,6 +12,7 @@ const config = {
 	],
 
 	kit: {
+		adapter: vercel(),
 		target: '#svelte',
 		router: false
 	}
