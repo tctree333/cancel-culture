1218b8a3941b1b0d4e834d2c0bab65c4d560e4e2
Tomi Chen <tctree333@users.noreply.github.com>
2021-05-29 21:28:06 -0700

    add error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,12 @@
+<h1 class="my-10 text-6xl leading-tight">The page was not found, or an error occurred.</h1>
+<p class="text-xl">
+	<a class="text-blue-800 underline dark:text-blue-300" href="https://iscancelled.com/"
+		>Go back to the main page.</a
+	>
+</p>
+
+<img
+	src="/404.jpg"
+	alt="a red panda on a tree branch"
+	class="rounded-md max-h-[80vh] mx-auto mt-16"
+/>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,38 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = async ({ page }) => {
+		return {
+			props: {
+				main: page.host.split('.').length === 2,
+				plural: page.host.includes('arecancelled.com'),
+				name: page.host
+					.split('.')[0]
+					.split('-')
+					.map((c) => c[0].toUpperCase() + c.substr(1).toLowerCase())
+					.join(' ')
+			}
+		};
+	};
+</script>
+
 <script lang="ts">
 	import '../app.postcss';
+
+	import DarkToggle from '$lib/components/DarkToggle.svelte';
+	import Head from '$lib/components/Head.svelte';
+
+	export let main: boolean;
+	export let plural: boolean;
+	export let name: string;
+
+	let title = main ? 'Cancel Anything!' : `${name} ${plural ? 'Are' : 'Is'} Cancelled!`;
+	let description = main
+		? ''
+		: `${name} ${plural ? 'are' : 'is'} cancelled! ` +
+		  `What terrible things did they do? Who knows! All we know is that ` +
+		  `${name} ${plural ? 'are' : 'is'} cancelled, and they probably deserve it! ` +
+		  `Join us as we celebrate the #${name.split(' ').join('')}${plural ? 'Are' : 'Is'}OverParty!`;
 </script>
 
 <svelte:head>
@@ -19,4 +52,25 @@
 	</script>
 </svelte:head>
 
-<slot />
+<Head {title} {description} index={main} />
+
+<div class="relative max-w-6xl mx-auto">
+	<div class="absolute top-0 right-0 mr-4 max-w-max">
+		<DarkToggle
+			widthClass="w-16 h-16"
+			class="bg-green-300 rounded-b-lg dark:bg-green-800"
+			size="32"
+		/>
+	</div>
+</div>
+
+<main class="min-h-full max-w-4xl px-6 py-4 mx-auto">
+	<slot {main} {plural} cancelledName={name} />
+</main>
+<footer
+	class="flex flex-col items-center justify-center w-full py-16 mt-20 text-gray-100 bg-gray-900 "
+>
+	<p class="text-lg">
+		A web project by <a class="underline" href="https://tomichen.com">Tomi Chen.</a>
+	</p>
+</footer>
