9a277d120814c045935f582601dde896f9d688d9
Tomi Chen <tctree333@users.noreply.github.com>
2021-05-31 19:49:06 -0700

    duplicated head tags

--- src/lib/components/Head.svelte
+++ src/lib/components/Head.svelte
@@ -28,9 +28,6 @@
 </script>
 
 <svelte:head>
-	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
 	<title>{title}</title>
 	<meta name="description" content={description} />
 
