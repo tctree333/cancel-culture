a78f02755be8f3abcc66954c83307c98090e5e9b
Tomi Chen <tctree333@users.noreply.github.com>
2021-05-31 19:43:31 -0700

    maybe we don't need to preload?

--- src/lib/components/Head.svelte
+++ src/lib/components/Head.svelte
@@ -34,14 +34,6 @@
 	<title>{title}</title>
 	<meta name="description" content={description} />
 
-	<link
-		rel="preload"
-		href="/fonts/PT-Root-UI_Regular-subset.woff2"
-		as="font"
-		type="font/woff2"
-		crossorigin="anonymous"
-	/>
-
 	<!-- Allow search engines to crawl certain pages -->
 	{#if index === false}
 		<meta name="robots" content="none" />
