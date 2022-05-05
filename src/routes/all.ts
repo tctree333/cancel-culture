cc1ed92f0b2f56c6a94452a845465fd7f92959f1
Tomi Chen <tctree333@users.noreply.github.com>
2022-05-04 22:38:48 -0700

    fix rate limit

--- src/routes/all.ts
+++ src/routes/all.ts
@@ -2,7 +2,7 @@ import { getLeaderboard } from '$lib/utils/db';
 import { rateLimit } from '$lib/utils/limit';
 import type { RequestHandler } from '@sveltejs/kit';
 
-const readLimiter = rateLimit(15, 1000 * 30, 'read'); // 15 reads every 30 seconds
+const readLimiter = rateLimit(15, 30, 'read'); // 15 reads every 30 seconds
 
 export const get: RequestHandler = async ({ request, url }) => {
 	const token = request.headers['x-real-ip'] || url.searchParams.get('token');
--- src/routes/inc.ts
+++ src/routes/inc.ts
@@ -2,8 +2,8 @@ import { bumpCount, getCount, getRank } from '$lib/utils/db';
 import { rateLimit } from '$lib/utils/limit';
 import type { RequestHandler } from '@sveltejs/kit';
 
-const writeLimiter = rateLimit(100, 1000 * 60 * 15, 'write'); // 100 writes every 15 minutes
-const readLimiter = rateLimit(15, 1000 * 30, 'read'); // 15 reads every 30 seconds
+const writeLimiter = rateLimit(100, 60 * 15, 'write'); // 100 writes every 15 minutes
+const readLimiter = rateLimit(15, 30, 'read'); // 15 reads every 30 seconds
 
 export const get: RequestHandler = async ({ request, url }) => {
 	const token = request.headers['x-real-ip'] || url.searchParams.get('token');
